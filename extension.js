8d16315109fcbed95f164d7f0b51f4a6365f56a9
Mark Lavercombe <mlava@users.noreply.github.com>
2022-09-03 17:24:43 +1000

    Bug fixes - Reading Time
    Fix no reading time div onload
    Updated to allow dynamic update of RT based on changes in Roam Depot settings

--- extension.js
+++ extension.js
@@ -1,19 +1,35 @@
-const config = {
-    tabTitle: "Reading Time",
-    settings: [
-        {
-            id: "rt-wpm",
-            name: "Words per Minute",
-            description: "Reading rate in words per minute",
-            action: { type: "input", placeholder: "238" },
-        },
-    ]
-};
+
 let hashChange = undefined;
+var wpm, minutes, seconds;
 
 export default {
     onload: ({ extensionAPI }) => {
+        const config = {
+            tabTitle: "Reading Time",
+            settings: [
+                {
+                    id: "rt-wpm",
+                    name: "Words per Minute",
+                    description: "Reading rate in words per minute",
+                    action: {
+                        type: "input", placeholder: "238",
+                        onChange: (evt) => { wpm = evt.target.value; checkRT(); }
+                    },
+                },
+            ]
+        };
         extensionAPI.settings.panel.create(config);
+
+        if (extensionAPI.settings.get("rt-wpm")) {
+            const regex = /^[0-9]{2,3}$/;
+            if (extensionAPI.settings.get("rt-wpm").match(regex)) {
+                wpm = extensionAPI.settings.get("rt-wpm");
+            } else {
+                sendConfigAlert();
+            }
+        } else {
+            wpm = "238"; // Brysbaert M. How many words do we read per minute? A review and meta-analysis of reading rate. 2019. doi:10.31234/osf.io/xynwg.
+        }
 
         async function sleep(ms) {
             return new Promise(resolve => setTimeout(resolve, ms));
@@ -21,47 +37,24 @@ export default {
 
         hashChange = async (e) => {
             await sleep(10);
-            checkRT();
+            checkRT({ extensionAPI });
         };
         window.addEventListener('hashchange', hashChange);
 
-        if (!document.getElementById('rtDiv')) {
-            var rtDiv = document.createElement('div');
-            rtDiv.innerHTML = "";
-            rtDiv.id = 'rtDiv';
-        }
-        var topBarContent = document.querySelector("#app > div > div > div.flex-h-box > div.roam-main > div.rm-files-dropzone > div");
-        var topBarRow = topBarContent.childNodes[1];
-
-        if (topBarContent && topBarRow) {
-            topBarRow.parentNode.insertBefore(rtDiv, topBarRow);
-        }
+        checkRT(); // onload
 
         async function checkRT() {
-            var wpm, minutes, seconds;
-            breakme: {
-                if (extensionAPI.settings.get("rt-wpm")) {
-                    const regex = /^[0-9]{2,3}$/;
-                    if (extensionAPI.settings.get("rt-wpm").match(regex)) {
-                        wpm = extensionAPI.settings.get("rt-wpm");
-                    } else {
-                        sendConfigAlert();
-                        break breakme;
-                    }
-                } else {
-                    wpm = "238"; // Brysbaert M. How many words do we read per minute? A review and meta-analysis of reading rate. 2019. doi:10.31234/osf.io/xynwg.
-                }
+            var wordCount = 0;
 
-                var wordCount = 0;
-                const startBlock = window.location.href.split('/')[7];
+            const startBlock = window.location.href.split('/')[7];
 
-                let ancestorrule = `[ 
+            let ancestorrule = `[ 
                     [ (ancestor ?b ?a) 
                             [?a :block/children ?b] ] 
                     [ (ancestor ?b ?a) 
                             [?parent :block/children ?b ] 
                             (ancestor ?parent ?a) ] ] ]`;
-                let blocks = window.roamAlphaAPI.q(`[ 
+            let blocks = window.roamAlphaAPI.q(`[ 
                     :find 
                         ?string
                     :in $ ?startBlock % 
@@ -71,37 +64,51 @@ export default {
                         (ancestor ?block ?page)
                     ]`, startBlock, ancestorrule);
 
-                blocks.map((data, index) => { return data[0]; }).join('\n');
+            blocks.map((data, index) => { return data[0]; }).join('\n');
 
-                for (var i = 0; i < blocks.length; i++) {
-                    wordCount = wordCount + blocks[i][0].split(" ").length;
-                }
+            if (document.getElementById('rtDiv')) {
+                document.getElementById('rtDiv').remove();
+            }
 
-                if (wordCount > 0) {
-                    minutes = Math.floor(Math.abs(wordCount / wpm));
-                    seconds = Math.floor((Math.abs(wordCount / wpm) * 60) % 60);
-                } else {
-                    minutes = 0;
-                    seconds = 0;
-                }
+            var rtDiv = document.createElement('div');
+            rtDiv.innerHTML = "";
+            rtDiv.id = 'rtDiv';
 
-                if (document.getElementById('rtSpan')) {
-                    var oldSpan = document.getElementById('rtSpan');
-                    oldSpan.remove();
-                }
-                var rtSpan = document.createElement('span');
-                rtSpan.classList.add('bp3-button', 'bp3-minimal', 'bp3-small', 'bp3-icon-time');
-                rtSpan.id = 'rtSpan';
-                if (seconds < 10) {
-                    var txt = document.createTextNode("" + minutes + ":0" + seconds + "");
-                } else {
-                    var txt = document.createTextNode("" + minutes + ":" + seconds + "");
-                }
-                rtSpan.appendChild(txt);
-                var rtDiv = document.getElementById('rtDiv');
-                if (rtDiv) {
-                    rtDiv.append(rtSpan);
-                }
+            var topBarContent = document.querySelector("#app > div > div > div.flex-h-box > div.roam-main > div.rm-files-dropzone > div");
+            var topBarRow = topBarContent.childNodes[1];
+
+            if (topBarContent && topBarRow) {
+                topBarRow.parentNode.insertBefore(rtDiv, topBarRow);
+            }
+
+            for (var i = 0; i < blocks.length; i++) {
+                wordCount = wordCount + blocks[i][0].split(" ").length;
+            }
+
+            if (wordCount > 0 && wpm > 0) { //fix incase of unset Roam Depot settings
+                minutes = Math.floor(Math.abs(wordCount / wpm));
+                seconds = Math.floor((Math.abs(wordCount / wpm) * 60) % 60);
+            } else {
+                minutes = 0;
+                seconds = 0;
+            }
+
+            if (document.getElementById('rtSpan')) {
+                var oldSpan = document.getElementById('rtSpan');
+                oldSpan.remove();
+            }
+            var rtSpan = document.createElement('span');
+            rtSpan.classList.add('bp3-button', 'bp3-minimal', 'bp3-small', 'bp3-icon-time');
+            rtSpan.id = 'rtSpan';
+            if (seconds < 10) {
+                var txt = document.createTextNode("" + minutes + ":0" + seconds + "");
+            } else {
+                var txt = document.createTextNode("" + minutes + ":" + seconds + "");
+            }
+            rtSpan.appendChild(txt);
+            var rtDiv = document.getElementById('rtDiv');
+            if (rtDiv) {
+                rtDiv.append(rtSpan);
             }
         }
     },
